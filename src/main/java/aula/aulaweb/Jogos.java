73bc6f8783c4270ff10152e4492fedb4d1680fec
Gustavo <gu-gu-pf@hotmail.com>
2020-07-16 21:16:36 -0300

    first commit

--- src/main/java/aula/aulaweb/Jogos.java
+++ src/main/java/aula/aulaweb/Jogos.java
@@ -7,6 +7,9 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 @AllArgsConstructor
 @NoArgsConstructor
@@ -16,7 +19,13 @@ public class Jogos {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     Long id;
+
+    @NotBlank
     String nome;
+    @Size(min = 3, message = "A categoria deve conter, no mínimo, 3 dígitos  ")
+    @NotBlank(message = "O campo não pode ser vazio")
     String categoria;
+    @Size(min = 4, max = 4, message = "A data deve conter 4 algarismos")
+    @NotNull(message = "O campo não pode ser vazio")
     String anoLancamento;
 }
--- src/main/java/aula/aulaweb/JogosController.java
+++ src/main/java/aula/aulaweb/JogosController.java
@@ -3,12 +3,15 @@ package aula.aulaweb;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @Controller
@@ -38,10 +41,14 @@ public class JogosController {
     }
 
     @RequestMapping(value = "/salvar", method = RequestMethod.POST)
-    public String cadastrar(@ModelAttribute Jogos jogo){
-        jogosService.add(jogo);
+    public String cadastrar(@ModelAttribute @Valid Jogos jogo, Errors errors){
+        if (errors.hasErrors()) {
+            return "adicionar";
+        } else {
+            jogosService.add(jogo);
+            return "redirect:/";
+        }
 
-        return "redirect:/";
     }
 
     @RequestMapping("/editar/{id}")
